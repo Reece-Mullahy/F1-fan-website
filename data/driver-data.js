ff54711313d05afdb05cb6e3312a0a0e88b35514
Reece Mullahy <reece.mullahy1@gmail.com>
2023-06-26 20:33:06 -0400

    Driver Standing Done

--- data/driver-data.js
+++ data/driver-data.js
@@ -3,120 +3,140 @@ const driverData = [
   name: 'Alex Albon',
   team: 'Williams',
   headshot: 'driver-pictures/alex-albon-williams-1.jpeg',
-  countryFlag: 'driver-flags/tn_th-flag.gif'
+  countryFlag: 'driver-flags/tn_th-flag.gif',
+  points: 7
 },
 {
   name: 'Carlos Sainz',
   team: 'Ferrari',
   headshot: 'driver-pictures/carlos-sainz-ferrari-1.jpeg',
-  countryFlag: 'driver-flags/tn_sp-flag.gif'
+  countryFlag: 'driver-flags/tn_sp-flag.gif',
+  points: 68
 },
 {
   name: 'Charles Leclerc',
   team: 'Ferrari',
   headshot: 'driver-pictures/charles-leclerc-ferrari-1.jpeg',
-  countryFlag: 'driver-flags/tn_mn-flag.gif'
+  countryFlag: 'driver-flags/tn_mn-flag.gif',
+  points: 54
 },
 {
   name: 'Esteban Ocon',
   team: 'Alpine',
   headshot: 'driver-pictures/esteban-ocon-alpine-1.jpeg',
-  countryFlag: 'driver-flags/tn_fr-flag.gif'
+  countryFlag: 'driver-flags/tn_fr-flag.gif',
+  points: 29
 },
 {
   name: 'Fernando Alonso',
   team: 'Aston Martin',
   headshot: 'driver-pictures/fernando-alonso.jpeg',
-  countryFlag: 'driver-flags/tn_sp-flag.gif'
+  countryFlag: 'driver-flags/tn_sp-flag.gif',
+  points: 117
 },
 {
   name: 'George Russell',
   team: 'Mercedes',
   headshot: 'driver-pictures/george-russell-mercedes-1.jpeg',
-  countryFlag: 'driver-flags/tn_uk-flag.gif'
+  countryFlag: 'driver-flags/tn_uk-flag.gif',
+  points: 65
 },
 {
   name: 'Kevin Magnussen',
   team: 'Haas F1 Team',
   headshot: 'driver-pictures/kevin-magnussen-haas-1.jpeg',
-  countryFlag: 'driver-flags/tn_da-flag.gif'
+  countryFlag: 'driver-flags/tn_da-flag.gif',
+  points: 2
 },
 {
   name: 'Lance Stroll',
   team: 'Aston Martin',
   headshot: 'driver-pictures/lance-stroll-aston-martin-raci-1.jpeg',
-  countryFlag: 'driver-flags/tn_ca-flag.gif'
+  countryFlag: 'driver-flags/tn_ca-flag.gif',
+  points: 37
 },
 {
   name: 'Lando Norris',
   team: 'McLaren',
   headshot: 'driver-pictures/lando-norris-mclaren-1.jpeg',
-  countryFlag: 'driver-flags/tn_uk-flag.gif'
+  countryFlag: 'driver-flags/tn_uk-flag.gif',
+  points: 12
 },
 {
   name: 'Lewis Hamilton',
   team: 'Mercedes',
   headshot: 'driver-pictures/lewis-hamilton-mercedes-1.jpeg',
-  countryFlag: 'driver-flags/tn_uk-flag.gif'
+  countryFlag: 'driver-flags/tn_uk-flag.gif',
+  points: 102
 },
 {
   name: 'Logan Sargeant',
   team: 'Williams',
   headshot: 'driver-pictures/logan-sargeant-williams-1.jpeg',
-  countryFlag: 'driver-flags/tn_us-flag.gif'
+  countryFlag: 'driver-flags/tn_us-flag.gif',
+  points: 0
 },
 {
   name: 'Max Verstappen',
   team: 'Red Bull Racing',
   headshot: 'driver-pictures/max-verstappen-red-bull-racing-1.jpeg',
   countryFlag: 'driver-flags/tn_nt-flag.gif',
+  points: 195
 },
 {
   name: 'Nico Hulkenberg',
   team: 'Haas F1 Team',
   headshot: 'driver-pictures/nico-hulkenberg-haas-1.jpeg',
-  countryFlag: 'driver-flags/tn_gm-flag.gif'
+  countryFlag: 'driver-flags/tn_gm-flag.gif',
+  points: 6
 },
 {
   name: 'Nyck de Vries',
   team: 'AlphaTauri',
   headshot: 'driver-pictures/nyck-de-vries-alphatauri-1.jpeg',
-  countryFlag: 'driver-flags/tn_nt-flag.gif'
+  countryFlag: 'driver-flags/tn_nt-flag.gif',
+  points: 0
 },
 {
   name: 'Oscar Piastri',
   team: 'McLaren',
   headshot: 'driver-pictures/oscar-piastri-mclaren-1.jpeg',
-  countryFlag: 'driver-flags/tn_as-flag.gif'
+  countryFlag: 'driver-flags/tn_as-flag.gif',
+  points: 5
 },
 {
   name: 'Pierre Gasly',
   team: 'Alpine',
   headshot: 'driver-pictures/pierre-gasly-alpine-1.jpeg',
-  countryFlag: 'driver-flags/tn_fr-flag.gif'
+  countryFlag: 'driver-flags/tn_fr-flag.gif',
+  points: 15
 },
 {
   name: 'Sergio Perez',
   team: 'Red Bull Racing',
   headshot: 'driver-pictures/sergio-perez-red-bull-racing-1.jpeg',
-  countryFlag: 'driver-flags/tn_mx-flag.gif'
+  countryFlag: 'driver-flags/tn_mx-flag.gif',
+  points: 126
 },
 {
   name: 'Valtteri Bottas',
   team: 'Alfa Romeo',
   headshot: 'driver-pictures/valtteri-bottas-alfa-romeo-1.jpeg',
-  countryFlag: 'driver-flags/tn_fi-flag.gif'
+  countryFlag: 'driver-flags/tn_fi-flag.gif',
+  points: 5
 },
 {
   name: 'Yuki Tsunoda',
   team: 'AlphaTauri',
   headshot: 'driver-pictures/yuki-tsunoda-alphatauri-1.jpeg',
-  countryFlag: 'driver-flags/tn_ja-flag.gif'
+  countryFlag: 'driver-flags/tn_ja-flag.gif',
+  points: 2
 },
 {
   name: 'Zhou Guanyu',
   team: 'Alfa Romeo',
   headshot: 'driver-pictures/zhou-guanyu-alfa-romeo-1.jpeg',
-  countryFlag: 'driver-flags/tn_ch-flag.gif'
+  countryFlag: 'driver-flags/tn_ch-flag.gif',
+  points: 4
 },
 ]
--- home-page-js/drivers.js
+++ home-page-js/drivers.js
@@ -1,5 +1,9 @@
 let driverInfo = '';
 
+driverData.sort(
+  (driver1, driver2) => (driver1.points < driver2.points) ? 1 : (driver1.points > driver2.points) ? -1 : 0
+);
+
 driverData.forEach((driver) =>{
   driverInfo += `
 <div class="driver-profile">
@@ -8,6 +12,7 @@ driverData.forEach((driver) =>{
     <div class="flag-absolute">
       <img class="driver-flag"  src="${driver.countryFlag}">
     </div>
+    <div class="driver-points">${driver.points} pts</div>
   </div>
   <div class="driver-info">
     <div class="driver-name">${driver.name}</div>
